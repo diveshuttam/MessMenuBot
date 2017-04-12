17367668ca12ef1cfce8cc88d9d47cde6ace6831
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2017-04-12 11:58:42 +0530

    menu via datewise files

--- bot.py
+++ bot.py
@@ -14,8 +14,14 @@ class GMT5(datetime.tzinfo):
         return datetime.timedelta(0)
 
 gmt5 = GMT5()
-
 pp = pprint.PrettyPrinter(indent=4)
+prdt=None
+Today=None
+Tomorrow=None
+Yesterday=None
+dt=None
+dt_yes=None
+dt_tom=None
 
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
@@ -24,24 +30,40 @@ def on_chat_message(msg):
     if(content_type!="text"):
             reply='Sorry, But, I only accept text :-(\nType or Click "/ " to see availible commands'
     else:
+        global dt
+        global dt_yes
+        global dt_tom
         dt=datetime.datetime.fromtimestamp(msg["date"],tz=gmt5)
         #dt=dt.astimezone()
-        ti=dt.time()
+        ti=dt.time()    
         #print(msg["date"])
         print(ti)
+        global prdt
 
-        reply=dt.strftime("Today is %A, %d %B %Y\n\n")
+        reply=dt.strftime("Today is %A, %d %b %Y\n\n")
+        #date change
+        if(prdt is None or prdt.date()!=dt.date()):
+            tdt=datetime.timedelta(days=1)
+            dt_yes=dt-tdt
+            dt_tom=dt+tdt
+            todayfile=dt.strftime("./menu/%d-%m-%Y.json")
+            yesterdayfile=dt_yes.strftime("./menu/%d-%m-%Y.json")
+            tomorrowfile=dt_tom.strftime("./menu/%d-%m-%Y.json")
+            print("in date change")
 
-        with open('./today.json') as json_data:
-            Today = json.load(json_data)
-            json_data.close()
-        with open('./yesterday.json') as json_data:
-            Yesterday = json.load(json_data)
-            json_data.close()
-        with open('./tomorrow.json') as json_data:
-            Tomorrow = json.load(json_data)
-            json_data.close()
-
+            with open(todayfile) as json_data:
+                global Today 
+                Today = json.load(json_data)
+                json_data.close()
+            with open(yesterdayfile) as json_data:
+                global Yesterday 
+                Yesterday= json.load(json_data)
+                json_data.close()
+            with open(tomorrowfile) as json_data:
+                global Tomorrow 
+                Tomorrow= json.load(json_data)
+                json_data.close()
+        prdt=dt
         k=''
 
         nextmeal=Today["breakfast"]
@@ -54,6 +76,7 @@ def on_chat_message(msg):
             reply1="Today's dinner:\n"
         if(ti.hour==20 and ti.minute>30 or ti.hour>20):
             nextmeal=Tomorrow["breakfast"]
+            reply=dt_tom.strftime("Tomorrow is %A, %d %b %Y\n\n")
             reply1="Tomorrow's Breakfast:\n"
 
         t=msg['text']
@@ -67,10 +90,12 @@ def on_chat_message(msg):
             reply+="Today's menu-\n"
 
         elif(t=="/yesterday"):
+            reply=dt_yes.strftime("Yesterday was %A, %d %b %Y\n\n")
             k=json.dumps(Yesterday,indent=0)
             reply+="Yesterday's menu- \n"
 
         elif(t=="/tomorrow"):
+            reply=dt_tom.strftime("Tomorrow is %A, %d %b %Y\n\n")
             k=json.dumps(Tomorrow,indent=0)
             reply+="Tomorrow's menu:\n"
 
